d13fe140c665ec637e13c7ef195359d85b50e2ea
xutuio <103910480+xutuio@users.noreply.github.com>
2022-04-19 20:58:27 +0800

    HOP02

---
+++ objects.ts
@@ -0,0 +1,13 @@
+let employee:{ id:number,name:string} = {
+    id: 377,
+    name: 'Kevin'
+};
+
+console.log(employee.id);
+console.log(employee.name);
+
+employee.id =25;
+employee.name = 'David';
+
+console.log(employee.id);
+console.log(employee.name);
